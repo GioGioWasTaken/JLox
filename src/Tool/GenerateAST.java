eac8f134ef9d5cbeb0fdeb9540a39ee767b32e90
GioGioWasTaken <iyar.segev@gmail.com>
2023-10-16 17:18:10 +0300

    Syntax tree implemented with visitor pattern AST printer finished

--- src/Tool/GenerateAST.java
+++ src/Tool/GenerateAST.java
@@ -29,6 +29,7 @@ public class GenerateAST {
         writer.println();
         writer.println("abstract class " + baseName + " {");
         // The AST classes.
+        defineVisitor(writer, baseName, types);
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim(); // use regex to get the class name and needed fields for each one of our types.
@@ -38,6 +39,19 @@ public class GenerateAST {
         writer.println("}");
         writer.close();
     }
+
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println(" interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println(" R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println(" }");
+    }
+
+
     private static void defineType(
             PrintWriter writer, String baseName,
             String className, String fieldList) {
@@ -51,8 +65,19 @@ public class GenerateAST {
             String name = field.split(" ")[1];
             writer.println(" this." + name + " = " + name + ";");
         }
+
+        // The base accept() method.
+        writer.println();
+        writer.println(" abstract <R> R accept(Visitor<R> visitor);");
         writer.println(" }");
-    // Fields.
+
+        // Visitor pattern.
+        writer.println();
+        writer.println(" @Override");
+        writer.println(" <R> R accept(Visitor<R> visitor) {");
+        writer.println(" return visitor.visit" + className + baseName + "(this);");
+        writer.println(" }");
+        // Fields.
         writer.println();
         for (String field : fields) {
             writer.println(" final " + field + ";");
